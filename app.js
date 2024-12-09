232147d7c4124345d5cf848ee8ade97a1737ede3
finteger <nashassociatesinc@gmail.com>
2024-12-09 11:20:37 -0700

    Update

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 const PORT = 3000;
 
 
-app.get('/api/load-testing', (req, res) =>{
-    res.json({message: `Request processed: ${req.body}`});
+app.get('/api/load-testing', (req, res) =>{ 
+    //Throttling
+    const delay = 2000;
+
+    setTimeout(() =>{
+        res.json({message: `Request processed: ${req.body}`});
+    }, delay);
+
 });
+
 
 app.listen(PORT, ()=>{
     console.log(`Connected to port ${PORT}`);
